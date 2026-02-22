3908f239d1022ec734348ff2f531650e7ac98917
a28138 <rbhatt@eastsideprep.org>
2026-02-22 11:14:29 -0800

    Add files via upload

--- habitat_button.py
+++ habitat_button.py
@@ -1,8 +1,16 @@
 import streamlit as st
-from trees import Fih, Bord
-city_list = ["Seattle","Bellevue","Issaquah","Auburn","Snoqualmie","Spokane"]
+
+city_list = ["Seattle","Issaquah","Auburn","Snoqualmie","Spokane"]
 user_city = st.selectbox("Put in the city you live closest to", city_list)
 
-if st.button("Habitat Finder!"):
-    if user_city == "Seattle":
-        st.write("")
+if user_city == "Seattle":
+    st.link_button("Go To Seattle Page", "")
+elif user_city == "Issaquah":
+    st.link_button("Go To Issaquah Page")
+elif user_city == "Auburn":
+    st.link_button("Go To Auburn Page")
+elif user_city == "Snoqualmie":
+    st.link_button("Go To Snoqualmie Page")
+elif user_city == "Spokane":
+    st.link_button("Go To Spokane Page")
+st.link_button("Go to the Birds Page")
--- mangcodersauburn.py
+++ mangcodersauburn.py
@@ -6,5 +6,3 @@ st.image(
     width=615,
     caption="Mountain Whitefish"
 )
-st.write("Mountain Whitefish are crucial to river ecosystems as a primary food source for birds of prey like eagles and ospreys. They also are indicators of high water quality and, as fish that feed on the bottom of the stream, play a vital role in nutrient cycling, which helps improve stream quality. To help protect their habitats, you can protect their cold-water habitats, practice sustainable fishing, and support conservation that removes invasive species.")
-st.sidebar.link_button("HI","a")
