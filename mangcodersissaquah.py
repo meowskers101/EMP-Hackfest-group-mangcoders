3e9f5163c604914b0f0cd002449e33b5a6ec6ced
a28138 <rbhatt@eastsideprep.org>
2026-02-22 08:35:05 -0800

    Add files via upload

---
+++ mangcodersissaquah.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Issaquah Fish")
+st.write("Here are some fish that primarily live in Issaquah, and how you can help their habitats.")
+st.write("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAMUBuog8BQczZgvHKmaeMqk9b4O3BqsiNdA&s", width=615, caption ="Coho Salmon")
+st.write("Coho Salmon are a key part in river ecosystems due to how they transport important nutrients, such as nitrogen and phosporous, from the ocean to their spawning ground, in turn fueling the forest ecosystems and wildlife around them. Due to the sheer amount of them travelling upstream, they make an excellent food source for predators like bears, eagles, and even orcas. You can help support Coho Salmon by helping build sustainable fish ladders to help them get to spawning grounds easier, and supporting stream restoration efforts. ")
