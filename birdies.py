1631b1cad7d46ed4934e5a01f4ffd7caf2e18dbb
a28138 <rbhatt@eastsideprep.org>
2026-02-22 11:20:53 -0800

    Add files via upload

---
+++ birdies.py
@@ -0,0 +1,105 @@
+import streamlit as st
+st.title("Year-round Washington Bird Residents")
+st.write("This is a list of all the birds that reside in Washington year-round.")
+st.image(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL4tLfInlUubHtkL0SIlfgNrq9dAsrQTkVPg&s",
+    width=615,
+    caption="Dark-eyed Junco"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkXQfpU4eFPYxOiZHrirvqrda-K3E2z5gq2A&s",
+    width=615,
+    caption="Song Sparrow"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://www.allaboutbirds.org/guide/assets/photo/305874101-240px.jpg",
+    width=615,
+    caption="House Sparrow"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://www.allaboutbirds.org/guide/assets/photo/66120991-480px.jpg",
+    width=615,
+    caption="Anna's Hummingbird"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://upload.wikimedia.org/wikipedia/commons/9/97/American_robin_%2871307%29.jpg",
+    width=615,
+    caption="American Robin"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://myedmondsnews.com/wp-content/uploads/2019/07/Ring-billed-Gull1-9-12-17.jpeg",
+    width=615,
+    caption="Ring-billed Gull"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://cdn.download.ams.birds.cornell.edu/api/v2/asset/362144561/1200",
+    width=615,
+    caption="Common Gull"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://www.allaboutbirds.org/guide/assets/photo/161342541-480px.jpg",
+    width=615,
+    caption="Golden-crowned Kinglet"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://images.squarespace-cdn.com/content/v1/5a6390338a02c77bf05da4ab/1568046711831-MFVDD3CDZXBTYDTON1HH/33462847703_72c5e23bb7_o.jpg",
+    width=615,
+    caption="Steller's Jay"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://upload.wikimedia.org/wikipedia/commons/b/bf/Anas_platyrhynchos_male_female_quadrat.jpg",
+    width=615,
+    caption="Mallard"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://cdn.britannica.com/92/152292-050-EAF28A45/Bald-eagle.jpg",
+    width=615,
+    caption="Bald Eagle"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://tucsonbirds.org/wp-content/uploads/2021/09/RTHA_Mick-Thompson-scaled.jpg",
+    width=615,
+    caption="Red-tailed Hawk"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://cdn.britannica.com/89/258789-050-23C427F1/Great-blue-heron-bird-ardea-herodias-Forsythe-National-Wildlife-Refuge-NJ.jpg",
+    width=615,
+    caption="Great Blue Heron"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://upload.wikimedia.org/wikipedia/commons/0/0a/Corvus-brachyrhynchos-001.jpg",
+    width=615,
+    caption="American Crow"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://couchichingconserv.ca/wp-content/uploads/2014/04/Hairy-Woodpecker-A.Stinnissen-706x653-1.jpg",
+    width=615,
+    caption="Hairy Woodpecker"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://www.allaboutbirds.org/guide/assets/photo/304473511-480px.jpg",
+    width=615,
+    caption="Bewick's Wren"
+)
+st.write("Description Placeholder")
+st.image(
+    "https://upload.wikimedia.org/wikipedia/commons/c/c5/Pacific_Wren_-_Vancouver_Is._%286842161146%29.jpg",
+    width=615,
+    caption="Pacific Wren"
+)
+st.write("Description Placeholder")
