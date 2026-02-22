a9e32d5c45f4555b01b95c87055561c4d38e363b
a28138 <rbhatt@eastsideprep.org>
2026-02-22 08:30:04 -0800

    Delete mangcodersspokane.py

--- mangcodersspokane.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-st.title("Spokane Fish")
-st.write("Here are some fish that primarily live in Spokane, and how you can help their habitats.")
-st.write("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpyE_Xgs6o_-wXhTZUu86yixIg_o1RMKWepw&s", width=115, caption ="Red-band Trout")
-st.write("Red-band Trout are a critical part of Spokane's fragile habitats. Their presence by itself signifies healthy waters, as they need very specific temperatures to thrive. They also help maintain biodiversity and reinforce food webs. Red-band Trout are unique in the fact that they serve as both predator and prey in their ecosystem. You can help support them by releasing any red-band trout you catch while fishing, and also avoiding spawning areas to help support the eggs.")
