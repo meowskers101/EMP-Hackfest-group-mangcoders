01cc6065e35b1f152c1fcffbd7c1f1298115b381
a28138 <rbhatt@eastsideprep.org>
2026-02-22 08:29:32 -0800

    Delete habitat_button.py

--- habitat_button.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from trees import Fih, Bord
-city_list = ["Seattle","Bellevue","Issaquah","Auburn","Snoqualmie","Spokane"]
-user_city = st.selectbox("Put in the city you live closest to", city_list)
-
-if st.button("Habitat Finder!"):
-    if user_city == "Seattle":
-        st.write("")
